aa19acf9d3f435388303bbe72818fc1ea2f65bb5
Sitam Sardar <76570320+sitamadex11@users.noreply.github.com>
2021-09-29 17:13:37 +0530

    All required Data classes are created

---
+++ app/src/main/java/com/sitamadex11/cinedroid/data/MovieList.kt
@@ -0,0 +1,45 @@
+package com.sitamadex11.cinedroid.data
+
+import com.google.gson.annotations.SerializedName
+
+data class MovieList(
+	@field:SerializedName("results")
+	val results: List<Movie?>? = null
+)
+
+data class Movie(
+
+	@field:SerializedName("overview")
+	val overview: String? = null,
+
+	@field:SerializedName("original_language")
+	val originalLanguage: String? = null,
+
+	@field:SerializedName("original_title")
+	val originalTitle: String? = null,
+
+	@field:SerializedName("video")
+	val video: Boolean? = null,
+
+	@field:SerializedName("title")
+	val title: String? = null,
+
+	@field:SerializedName("poster_path")
+	val posterPath: String? = null,
+
+	@field:SerializedName("backdrop_path")
+	val backdropPath: String? = null,
+
+	@field:SerializedName("release_date")
+	val releaseDate: String? = null,
+
+	@field:SerializedName("popularity")
+	val popularity: Double? = null,
+
+	@field:SerializedName("id")
+	val id: Int? = null,
+
+	@field:SerializedName("adult")
+	val adult: Boolean? = null,
+
+)
---
+++ app/src/main/java/com/sitamadex11/cinedroid/data/TVShowList.kt
@@ -0,0 +1,38 @@
+package com.sitamadex11.cinedroid.data
+
+import com.google.gson.annotations.SerializedName
+
+data class TVShowList(
+	@field:SerializedName("results")
+	val results: List<TVShow?>? = null
+)
+
+data class TVShow(
+
+	@field:SerializedName("first_air_date")
+	val firstAirDate: String? = null,
+
+	@field:SerializedName("overview")
+	val overview: String? = null,
+
+	@field:SerializedName("original_language")
+	val originalLanguage: String? = null,
+
+	@field:SerializedName("poster_path")
+	val posterPath: String? = null,
+
+	@field:SerializedName("origin_country")
+	val originCountry: List<String?>? = null,
+
+	@field:SerializedName("backdrop_path")
+	val backdropPath: String? = null,
+
+	@field:SerializedName("original_name")
+	val originalName: String? = null,
+
+	@field:SerializedName("name")
+	val name: String? = null,
+
+	@field:SerializedName("id")
+	val id: Int? = null,
+)
